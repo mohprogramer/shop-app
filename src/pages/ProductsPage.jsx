33eddc568b1cdd2c45602e138313aa3d2ed6c61b
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-12-16 23:08:51 +0330

    test data

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,15 @@
 import { useProducts } from "../context/ProductContext";
 
 function ProductsPage() {
-  console.log(useProducts())
-  return <div>rpo</div>;
+  const products = useProducts();
+  return (
+    <div>
+      <div>{products.map(item => (
+        <p key={item.id} >{item.title}</p>
+      ))}</div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
