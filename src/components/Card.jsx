df3a5309694f76497daeb6f6b62c5bd9f646d52b
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-12-17 21:12:47 +0330

    card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,31 @@
-function Card() {
-    return (
+import { Link } from "react-router-dom";
+//Icons
+import { TbListDetails } from "react-icons/tb";
+import { TbShoppingBagCheck } from "react-icons/tb";
+//helpers
+import { shortenText } from "../helper/helper";
+//styles
+import styles from "./Card.module.css";
+
+function Card({ data }) {
+  const { id, image, title, price } = data;
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} />
+      <h3>{shortenText(title)}</h3>
+      <p> ${price}</p>
+      <div className={styles.actions}>
+        <Link to={`/products/${id}`}>
+          <TbListDetails />
+        </Link>
         <div>
-            Card
+          <button>
+            <TbShoppingBagCheck />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Card;
