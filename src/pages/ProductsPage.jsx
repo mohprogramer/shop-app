20aec2af89c1f7be11e9bd4a6ef3cf78b62a0f99
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-12-17 20:34:54 +0330

    base styles

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,17 @@
 import { useProducts } from "../context/ProductContext";
+import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
   return (
-    <div>
-      <div>{products.map(item => (
-        <p key={item.id} >{item.title}</p>
-      ))}</div>
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p>}
+
+        {products.map((item) => (
+          <p key={item.id}>{item.title}</p>
+        ))}
+      </div>
       <div>sidebar</div>
     </div>
   );
