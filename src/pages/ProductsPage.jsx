3a12e2c71900f4bee61c48de4a9acb15ad524ddc
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-12-18 12:40:33 +0330

    category

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,14 @@
+import { useState } from "react";
 import { useProducts } from "../context/ProductContext";
 //Icons
 import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 //styles
 import styles from "./ProductsPage.module.css";
 //components
 import Card from "../components/Card";
 //Loader
 import Loader from "../components/Loader";
-import { useState } from "react";
 
 function ProductsPage() {
   const products = useProducts();
@@ -15,6 +16,14 @@ function ProductsPage() {
 
   const searchHandler = () => {
     console.log("search");
+  };
+
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+
+    if (tagName != "LI") return;
+    console.log(category);
   };
 
   return (
@@ -38,7 +47,19 @@ function ProductsPage() {
             <Card key={item.id} data={item} />
           ))}
         </div>
-        <div>sidebar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Caregories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
     </>
   );
