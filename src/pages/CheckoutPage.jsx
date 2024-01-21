e66fa7235aba4fc6f4a5aaa10d11b0704507a211
Mohammad Davodi <mohammaddavodi96@gmail.com>
2024-01-21 22:44:31 +0330

    Basket

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,14 +1,26 @@
 import { useCart } from "../context/CartContext";
 import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSidebar";
+//styles
+import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
   const [state, dispatch] = useCart();
 
   const clickHandler = (type, payload) => dispatch({ type, payload });
 
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container} >
+        <p>Khali</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products} >
         {state.selectedItems.map((item) => (
           <BasketCard key={item.id} data={item} clickHandler={clickHandler} />
         ))}
